ff03d69fd053b1a7c7a7c9fe6e6f1d47c90555e0
ilyatoka <ilyatoka@yahoo.com>
2015-12-20 01:55:42 +0200

    Initial project setup

---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by ILYA Toka on 12/20/15.
+//  Copyright © 2015 ILYA Toka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
